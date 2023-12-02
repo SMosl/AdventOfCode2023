e72cea1d1143a442442fcc4aa889ff109879dd2f
Sam Mosley <sam.mosley@gmail.com>
2023-12-02 08:57:39 +0000

    Solve Day 2

---
+++ Day_02/Day2.py
@@ -0,0 +1,34 @@
+import os
+import time
+import re
+
+def main(part):
+
+	dir_path = os.path.dirname(os.path.realpath(__file__))
+	with open(f"{dir_path}/input.txt", "r") as f:
+		output = [0,0]
+		while(len(line := f.readline().strip()) != 0):
+			ID = int(re.search('\d+', line.split(':')[0]).group())
+			line_data = line.split(':')[1]
+			
+			blue = max([int(re.search('\d+', x).group()) for x in re.findall(r'(\d+ blue)', line_data)])
+			red = max([int(re.search('\d+', x).group()) for x in re.findall(r'(\d+ red)', line_data)])
+			green = max([int(re.search('\d+', x).group()) for x in re.findall(r'(\d+ green)', line_data)])
+
+			if (red <= 12) & (green <= 13) & (blue <= 14):
+				output[0] += ID
+			
+			output[1] += red * green * blue
+
+	if part == 1:
+		return output[0]
+	else:
+		return output[1]
+
+if __name__ == "__main__":
+	start_time = time.time()
+	print(f" Part 1 solution: {main(1)}")
+	print("Part 1 finished --- %s seconds ---" % (time.time() - start_time))
+	mid_time = time.time()
+	print(f" Part 2 solution: {main(2)}")
+	print("Part 2 finished --- %s seconds ---" % (time.time() - mid_time))
