6b6d2823fbcc8c7ab95226415e91ce354610f1ca
Sam Mosley <sam.mosley@gmail.com>
2023-12-06 09:51:30 +0000

    Solve Day 6

--- Day_06/Day6.py
+++ Day_06/Day6.py
@@ -3,30 +3,27 @@ import time
 import numpy as np
 
 def main(part):
-
 	dir_path = os.path.dirname(os.path.realpath(__file__))
 	with open(f"{dir_path}/input.txt", "r") as f:
 		input_data = f.read().splitlines()
-	times_1 = [int(x) for x in input_data[0].split()[1:]]
-	distances_1 = [int(x) for x in input_data[1].split()[1:]]
-	times_2 = int(''.join([x for x in input_data[0].split()[1:]]))
-	distances_2 = int(''.join([x for x in input_data[1].split()[1:]]))
-	
-	if part == 1:
-		total_ways = []
-		for i in range(len(times_1)):
-			ways = 0
-			for j in range(times_1[i]):
-				if (times_1[i] - j) * j > distances_1[i]:
-					ways += 1
-			total_ways.append(ways)
-		return(np.product(total_ways))
-	else:
-		ways = 0
-		for i in range(times_2):
-			if (times_2 - i) * i > distances_2:
-				ways += 1
-		return(ways)
+	times = [[int(x) for x in input_data[0].split()[1:]], [int(''.join([x for x in input_data[0].split()[1:]]))]]
+	distances = [[int(x) for x in input_data[1].split()[1:]], [int(''.join([x for x in input_data[1].split()[1:]]))]]
+
+	total_ways = []
+	for i, t in enumerate(times[part - 1]):
+		x = ((-t + np.sqrt(t**2 - (4 * distances[part - 1][i]))) / -2, 
+	   		(-t - np.sqrt(t**2 - (4 * distances[part - 1][i]))) / -2)
+		if np.ceil(min(x)) == min(x):
+			lb = min(x) + 1
+		else:
+			lb = np.ceil(min(x))
+
+		if np.floor(max(x)) == max(x):
+			ub = max(x) - 1
+		else:
+			ub = np.floor(max(x))
+		total_ways.append(ub - lb + 1)
+	return(np.product(total_ways))
 
 if __name__ == "__main__":
 	start_time = time.time()
